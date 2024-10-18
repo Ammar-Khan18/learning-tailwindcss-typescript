e0dce78db6343de5decdcd48e4f02065305d104d
Ammar Khan <ammar.khaan05@gmail.com>
2024-10-18 19:45:57 +0500

    modified to include the sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl text-teal-500 font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
   );
 }
